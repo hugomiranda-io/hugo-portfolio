1dc2569648d57dfbb76578ea3d68d83b2ac1a9ac
Hugo Miranda <hugocsmiranda@gmail.com>
2026-02-04 18:29:06 +0100

    add cv and download button

--- src/components/organisms/IntroText/IntroText.tsx
+++ src/components/organisms/IntroText/IntroText.tsx
@@ -1,13 +1,26 @@
 import "./IntroText.scss";
-import { Text } from "@components";
+import { Download } from "lucide-react";
+
+import { Cta, Text } from "@components";
 import { INTROTEXT_DATA } from "@data";
 
 export default function IntroText() {
+ const cvPath = "/cv/CV_HugoMiranda_DevFrontEnd_UiDesigner.pdf";
+
  return (
-  <div className="flex flex-col gap-xs">
-   {INTROTEXT_DATA.text.map((paragraph, index) => (
-    <Text key={index} as="p" text={paragraph} color="muted" />
-   ))}
-  </div>
+  <>
+   <div className="flex flex-col gap-xs">
+    {INTROTEXT_DATA.text.map((paragraph, index) => (
+     <Text key={index} as="p" text={paragraph} color="muted" />
+    ))}
+   </div>
+   <Cta
+    label="Download Cv"
+    leftIcon={Download}
+    as="a"
+    href={cvPath}
+    download="CV_HugoMiranda_DevFrontEnd_UiDesigner.pdf"
+   />
+  </>
  );
 }
